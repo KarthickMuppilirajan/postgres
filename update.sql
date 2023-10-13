b10066eecc36522c4dd95c64b36dd867497ef3d1
Karthick Muppilirajan <veeramani.karthick@fourbends.com>
2023-10-13 12:30:11 +0530

    added update.sql

---
+++ update.sql
@@ -0,0 +1,23 @@
+CREATE TABLE pqbs_test (
+    id serial PRIMARY KEY,
+    course_name VARCHAR(50),
+    course_creation_date DATE,
+);
+
+CREATE TABLE pqbs_test_2 (
+    id serial PRIMARY KEY,
+    course_name VARCHAR(50),
+    course_creation_date DATE,
+);
+
+CREATE TABLE pqbs_test_3 (
+    id serial PRIMARY KEY,
+    course_name VARCHAR(50),
+    course_creation_date DATE,
+);
+
+CREATE TABLE pqbs_test_4 (
+    id serial PRIMARY KEY,
+    course_name VARCHAR(50),
+    course_creation_date DATE,
+);
