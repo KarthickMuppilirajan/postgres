b03d3759d31bae0635f490bb041308083be80808
Karthick Muppilirajan <veeramani.karthick@fourbends.com>
2023-10-13 13:01:35 +0530

    Updated the tables in update.yml

--- update.sql
+++ update.sql
@@ -1,23 +1,25 @@
-CREATE TABLE pqbs_test (
-    id serial PRIMARY KEY,
-    course_name VARCHAR(50),
-    course_creation_date DATE,
+-- Create tables with "pqbs" prefix
+
+-- Table 1
+CREATE TABLE pqbs_users (
+    user_id serial PRIMARY KEY,
+    username VARCHAR (50) NOT NULL,
+    email VARCHAR (100) NOT NULL
 );
 
-CREATE TABLE pqbs_test_2 (
-    id serial PRIMARY KEY,
-    course_name VARCHAR(50),
-    course_creation_date DATE,
+-- Table 2
+CREATE TABLE pqbs_products (
+    product_id serial PRIMARY KEY,
+    product_name VARCHAR (100) NOT NULL,
+    price numeric(10, 2) NOT NULL
 );
 
-CREATE TABLE pqbs_test_3 (
-    id serial PRIMARY KEY,
-    course_name VARCHAR(50),
-    course_creation_date DATE,
+-- Table 3
+CREATE TABLE pqbs_orders (
+    order_id serial PRIMARY KEY,
+    user_id INT REFERENCES pqbs_users(user_id),
+    order_date DATE NOT NULL,
+    total_amount numeric(10, 2) NOT NULL
 );
 
-CREATE TABLE pqbs_test_4 (
-    id serial PRIMARY KEY,
-    course_name VARCHAR(50),
-    course_creation_date DATE,
-);
+-- Add more tables with the "pqbs" prefix as needed
